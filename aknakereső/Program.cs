1e0399783484421a824f02fddba2be4b88aaf73f
juhász.ábel.kristóf <juhasz.abel99@gmail.com>
2020-02-04 13:55:58 +0100

    pálya kirajzolása és feltöltése

--- aknakereső/Program.cs
+++ aknakereső/Program.cs
@@ -7,17 +7,35 @@ using System.Threading.Tasks;
 namespace aknakereső
 {
     class Program
-    {
-        char[,] pálya = new char[10,10]; 
+    { 
         static void Main(string[] args)
         {
-            Feltöltés();
-            
+            char[,] pálya = new char[10,10]; 
+            Feltöltés(pálya);
+            Kirajzoló(pálya);
             Console.ReadKey();
         }
-        static void Feltöltés()
+        static void Kirajzoló(char[,] pálya)
+        {           
+            for (int sor = 0; sor < pálya.GetLength(0); sor++)   
+            {
+                for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)     
+                {
+                    Console.Write($"{pálya[sor, oszlop]}|");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Feltöltés(char[,] pálya)
         {
-            
+            for (int sor = 0; sor < pálya.GetLength(0); sor++)   
+            {
+                for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)     
+                {
+                    pálya[sor, oszlop] = '_';
+                }
+            }
         }
     }
 }
